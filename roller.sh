8d0cd23f0ac224f1ef75578611ce26194aef06b0
monyslim <prcenturionboy@gmail.com>
2023-12-16 16:11:01 +0100

    test-update

---
+++ roller.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# install the runner
+mkdir /home/ubuntu/actions-runner
+cd /home/ubuntu/actions-runner
+curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
+echo -ne "\n" |./config.sh --url https://github.com/monyslim/Logistical-pipeline --token A3KRTZ4DT62BPYIYCHWXZHTFPW7CU
+./run.sh&
